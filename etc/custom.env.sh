b3a8ae1d79da4b805c2c3c59468a0a21105a3c4f
Ky-Anh Huynh <kyanh@viettug.org>
2019-01-19 17:52:12 +0700

    etc/custom.env.sh: change default settings

--- etc/custom.env.sh
+++ etc/custom.env.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Number of workers
-N_WORKERS=2
+N_WORKERS=1
 # Number of controllers. Use 2 and expect to see split-brain issue ;)
-N_CONTROLLERS=2
+N_CONTROLLERS=1
 # Memory for worker instance. Can use lower size.
-MEM_WORKER="1024"
+MEM_WORKER="2024"
 # Memory for controllers. 1g is recommened.
 MEM_CONTROLLER="1024"
 # Our haproxy and adhoc dns resolver. 256mb is just enough.
